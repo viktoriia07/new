a3e135b527f9930de4c41018cf0b685e52e13485
V <ler.vi@gmail.com>
2022-01-23 19:25:14 +0100

    comn

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// alert('Hello');
+
+// let result = confirm("You here ?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18 ?", "");
+// console.log(typeof(answer)); 
+
+// const answers = [];
+
+// // answers [0] = prompt('What is your name?', '');
+// // answers [1] = prompt('What is your lastname?', '');
+// // answers [2] = prompt('How old you?', '');
+
+// console.log(typeof(answers));
+
+// let inc = 10,
+//     dec = 10;
+
+//     // ++inc;
+//     // --dec;
+
+//     console.log(inc++);
+//     console.log(dec--);
+
+    // console.log(2*4 == 8);
+
+    const isCheck = true,
+          isClose = false;
+
+          console.log(isCheck || isClose);
+        
